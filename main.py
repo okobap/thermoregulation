ca2b9b9c7b7b191f084ead41378507fb75cefa4a
okobap <baptiste.vauleon@okomera.com>
2022-06-10 10:30:36 +0200

    add LED blink

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import pyfirmata
+from pyfirmata import Arduino
+import time
 
-print('hello')
 
-print("trying new things")
+print('Test')
+
+
+
+if __name__ == '__main__':
+    board = Arduino('COM4') # establishing contact with the Arduino board via the choosen port 
+    print("Communication Successfully started")
+    
+    for i in range(5):
+        board.digital[13].write(1)
+        time.sleep(10)
+        print("LED set on")
+        board.digital[13].write(0)
+        time.sleep(1)
+        
