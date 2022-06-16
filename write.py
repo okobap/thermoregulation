be0c3176cae2d3bf292e6d05c2d8569d8f1355d9
okobap <baptiste.vauleon@okomera.com>
2022-06-16 09:12:16 +0200

    working storage of output data

--- write.py
+++ write.py
@@ -3,10 +3,11 @@
 ##############
 ## requires pySerial to be installed 
 import serial  # sudo pip install pyserial should work
+import time
 
 serial_port = 'COM3'
 baud_rate = 9600; #In arduino, Serial.begin(baud_rate)
-write_to_file_path = "output.txt"
+write_to_file_path = "data/" + input("Nom du fichier .txt de données de températures : ") + ".txt"
 
 output_file = open(write_to_file_path, "w+")
 ser = serial.Serial(serial_port, baud_rate)
@@ -15,4 +16,6 @@ while True:
     line = ser.readline()
     line = line.decode("utf-8") #ser.readline returns a binary, convert to string
     print(line)
+    output_file.write("\n" + str(time.time()) + "\n")
     output_file.write(line)
+    
